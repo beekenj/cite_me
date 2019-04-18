e90180d6366728da6a6413d66005559e3d25f551
JRiley4141 <42786690+JRiley4141@users.noreply.github.com>
2019-04-17 20:40:40 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -7,9 +7,6 @@
 ***********************/
 var express = require('express'); //Ensure our express framework has been added
 var app = express();
-var speakeasy =  require('speakeasy'); //Ensure our QR can be imaged
-var passport = require('passport'); //allows for passport to be used
-var QRCode = require('qrcode'); //allows for the creation of 
 var bodyParser = require('body-parser'); //Ensure our body-parser tool has been added
 app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
@@ -19,11 +16,11 @@ var pgp = require('pg-promise')();
 
 /**********************
   Database Connection information
-  host: This defines the ip address of the server hosting our database.  
-  port: This defines what port we can expect to communicate to our database.  
-  database: This is the name of our specific database.  
-  user: User is set by Heroku
-  password: This the password for accessing the database.  Password is set by Heroku!
+  host: This defines the ip address of the server hosting our database.  We'll be using localhost and run our database on our local machine (i.e. can't be access via the Internet)
+  port: This defines what port we can expect to communicate to our database.  We'll use 5432 to talk with PostgreSQL
+  database: This is the name of our specific database.  from our previous lab, we created the football_db database, which holds our football data tables
+  user: This should be left as postgres, the default user account created when PostgreSQL was installed
+  password: This the password for accessing the database.  You'll need to set a password USING THE PSQL TERMINAL THIS IS NOT A PASSWORD FOR POSTGRES USER ACCOUNT IN LINUX!
 **********************/
 const dbConfig = {
   host: 'ec2-54-225-129-101.compute-1.amazonaws.com',
@@ -44,10 +41,4 @@ app.get('/registeration', function(req, res) {
   res.render('pages/register',{
     my_title:"Registration Page"
   });
-});
-
-app.get(/index', function(req, res) {
-    res.render('pages/index', {
-        my_title: "Login Page"
-        });
 });
