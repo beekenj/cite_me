f2bce10d580b5ea96ef6a1ebc5e85a08e2744e46
jepa7365 <46698876+jepa7365@users.noreply.github.com>
2019-04-17 19:21:51 -0600

    Update proj.js

--- proj.js
+++ proj.js
@@ -1,7 +1,3 @@
-var passport = require('passport');
-var QRCode = require('qrcode');
-var speakeasy = require('speakeasy'); //Ensure our speakeasy framework has been added
-
 var secret = speakeasy.generateSecret({length: 20}); //gives a secret key of 32 characters
 
 function Display()
@@ -18,7 +14,7 @@ function Verify(input)
 {
 	var base32secret = two_factor_temp_secret;
 	// Use verify() to check the token against the secret
-	var verified = speakeasy.totp.verify({ secret: base32secret,
-										   encoding: 'base32',
-                                           token: input });
+	var verified = speakeasy.totp.verify({ secret: base32secret, 
+					       encoding: 'base32',
+                                               token: input });
 }
