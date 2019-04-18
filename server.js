a1d9bb0cb4c62a43d3e966be9c2a0d61534f6862
JRiley4141 <42786690+JRiley4141@users.noreply.github.com>
2019-04-18 09:46:54 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -12,31 +12,54 @@ app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 //Create Database Connection
-var pgp = require('pg-promise')();
+const { Pool, Client } = require('pg')
+const connectionString = 'postgres://brlletohmrgqrv:4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d1pjf7j45lf5pj'
+
+const pool = new Pool({
+  connectionString: connectionString,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
 
 /**********************
-  Database Connection information
-  host: This defines the ip address of the server hosting our database.  Heroku sets the host
-  port: This defines what port we can expect to communicate to our database.  Heroku sets the port
-  database: This is the name of our specific database in heroku 
-  user: Heroku sets the username
-  password: This the password for accessing the database.  Heroku sets the username
-**********************/
-const dbConfig = {
-  host: 'ec2-54-225-129-101.compute-1.amazonaws.com',
-  port: 5432,
-  database: 'd1pjf7j45lf5pj',
-  user: 'brlletohmrgqrv',
-  password: '4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7'
-};
+  Database Connection information: All set by Heroku
+  host: ec2-54-225-129-101.compute-1.amazonaws.com
+  port: 5432
+  database: d1pjf7j45lf5pj
+  user: brlletohmrgqrv
+  password: 4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7
+  URI: postgres://brlletohmrgqrv:4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d1pjf7j45lf5pj
 
-var db = pgp(dbConfig);
+**********************/
+
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/'));//This line is necessary for us to use relative paths and access our resources directory
 
-// registration page 
+// Homepage 
+app.get('/Homepage', function(req, res) {
+  res.render('pages/home',{
+    my_title:"Homepage"
+  });
+});
+
+
+// Registration page 
 app.get('/registeration', function(req, res) {
   res.render('pages/registration',{
     my_title:"Registration Page"
