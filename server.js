887f6065c27b9d60378ea648eb0f54a32ff3b57e
JRiley4141 <42786690+JRiley4141@users.noreply.github.com>
2019-04-19 00:23:35 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -19,31 +19,7 @@ app.use(bodyParser.json());              // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 
 
-//Create Database Connection
-const { Pool, Client } = require('pg')
-const connectionString = 'postgres://brlletohmrgqrv:4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7@ec2-54-225-129-101.compute-1.amazonaws.com:5432/d1pjf7j45lf5pj'
 
-const pool = new Pool({
-  connectionString: connectionString,
-})
-
-pool.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  pool.end()
-})
-
-const client = new Client({
-  connectionString: connectionString,
-})
-client.connect()
-
-client.query('SELECT NOW()', (err, res) => {
-  console.log(err, res)
-  client.end()
-})
-
-
-/*
 //Create Database Connection
 var pgp = require('pg-promise')();
 
@@ -54,14 +30,14 @@ var pgp = require('pg-promise')();
   database: d1pjf7j45lf5pj
   user: brlletohmrgqrv
   password: 4d8bbac5a3ba0b0666f61c1949c16c07ea72c6980a05eba8d413e288dfa7ffa7
-
+**********************/
 
 const dbConfig = process.env.DATABASE_URL;
 
 
 var db = pgp(dbConfig);
 
-*/
+
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
