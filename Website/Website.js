0ae2e1da38990d3d173f8180d1e47db64ac0b3cc
mlay0797 <45058074+mlay0797@users.noreply.github.com>
2019-04-22 17:16:29 -0600

    Add files via upload

--- Website/Website.js
+++ Website/Website.js
@@ -2,7 +2,7 @@
 
 
 
-function openCity(evt, cityName) {
+function openTab(evt, tabName) {
   // Declare all variables
   var i, tabcontent, tablinks;
 
@@ -19,6 +19,6 @@ function openCity(evt, cityName) {
   }
 
   // Show the current tab, and add an "active" class to the button that opened the tab
-  document.getElementById(cityName).style.display = "block";
+  document.getElementById(tabName).style.display = "block";
   evt.currentTarget.className += " active";
 }
